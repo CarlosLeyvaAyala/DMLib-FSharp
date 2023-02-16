d7cdeeb68e38ceb2a8e53e87363b931a40958b4f
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-02-16 00:42:52 -0600

    Add math functios

--- MathL.fs
+++ MathL.fs
@@ -2,11 +2,34 @@
 
 open System
 
+[<CompiledName("Max")>]
+let inline max<'a when 'a: comparison> a b : 'a = if a > b then a else b
+
+[<CompiledName("Min")>]
+let inline min<'a when 'a: comparison> a b : 'a = if a < b then a else b
+
+[<CompiledName("ForceMax")>]
+let forceMax = min
+
+[<CompiledName("ForceMin")>]
+let forceMin = max
+
+[<CompiledName("ForceRange")>]
+let forceRange lo hi x = x |> forceMax hi |> forceMin lo
+
+[<CompiledName("IsInRange")>]
+let inline isInRange<'a when 'a: comparison> (lo: 'a) hi x : bool = x >= lo && x <= hi
+
 type PointF = { X: float; Y: float }
 
-let ForcePositive x = max 0.0 x
-let ForcePercent x = Math.Clamp(0.0, 1, x)
-let PercentToFloat x = x / 100.0
+[<CompiledName("ForcePositive")>]
+let forcePositive x = max 0.0 x
+
+[<CompiledName("ForcePercent")>]
+let forcePercent x = Math.Clamp(0.0, 1, x)
+
+[<CompiledName("PercentToFloat")>]
+let percentToFloat x = x / 100.0
 
 [<CompiledName("LinCurve")>]
 let linCurve p1 p2 =
@@ -15,3 +38,16 @@ let linCurve p1 p2 =
     let m = (p2.Y - y1) / (p2.X - x1)
 
     fun x -> m * (x - x1) + y1
+
+[<CompiledName("ExpCurve")>]
+let expCurve shape p1 p2 x =
+    let e = Math.Exp
+    let b = shape
+    let ebx1 = e (b * p1.X)
+
+    match (e (b * p2.X)) - ebx1 with
+    | x when x = 0 -> linCurve p1 p2 x // Shape is actually a line, not an exponential curve.
+    | d ->
+        let a = (p2.Y - p1.Y) / d
+        let c = p1.Y - a * ebx1
+        a * (e (b * x)) + c
