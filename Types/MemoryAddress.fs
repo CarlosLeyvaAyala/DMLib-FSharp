8b6e3ec42794fd346812a0b99917a4e323082fa0
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-02-17 00:49:25 -0600

    Convert HexNumber to MemoryAddress

---
+++ Types/MemoryAddress.fs
@@ -0,0 +1,73 @@
+﻿namespace DMLib.Types
+
+open System.Text.RegularExpressions
+open DMLib.String
+open System
+
+[<CustomEquality; CustomComparison>]
+type MemoryAddress =
+    | Hex of string
+    static member Create(x) = Hex x
+    override h.GetHashCode() = h.ToString() |> hash
+
+    member h.ToInt() =
+        let (Hex n) = h in Int32.Parse(n, Globalization.NumberStyles.HexNumber)
+
+    member h.ToInt64() =
+        let (Hex n) = h in Int64.Parse(n, Globalization.NumberStyles.HexNumber)
+
+    member h.ToByte() =
+        let (Hex n) = h in Byte.Parse(n, Globalization.NumberStyles.HexNumber)
+
+    member h.ToUInt() =
+        let (Hex n) = h in UInt32.Parse(n, Globalization.NumberStyles.HexNumber)
+
+    member h.ToUInt64() =
+        let (Hex n) = h in UInt64.Parse(n, Globalization.NumberStyles.HexNumber)
+
+    override h.Equals(a) =
+        match a with
+        | :? MemoryAddress as x -> x.ToUInt64() = h.ToUInt64()
+        | _ -> false
+
+    interface System.IComparable with
+        member h.CompareTo a =
+            match a with
+            | :? MemoryAddress as x -> compare (h.ToUInt64()) (x.ToUInt64())
+            | :? uint64 as x -> compare (h.ToUInt64()) x
+            | _ -> invalidArg (nameof a) "Can not compare to this type."
+
+[<AutoOpen>]
+module MemoryAddressTopLevelOperations =
+    let validateStr s =
+        if isNullOrEmpty s then
+            invalidArg (nameof s) $"A memory address can not be an empty string."
+
+        s
+
+    let getValue s =
+        let non0x = Regex("(0[xX])?(.*)").Match(s).Groups[2].Value
+
+        if Regex("[^a-fA-F0-9]").Match(non0x).Success then
+            invalidArg (nameof s) $"{s} is not a valid memory address."
+
+        non0x
+
+    let removeTrailingZeros s =
+        Regex("(0*)(.*)").Match(s).Groups[2].Value
+
+    // Constructor
+    let MemoryAddress (s: obj) =
+        let v =
+            match s with
+            | :? byte as x -> x.ToString("X")
+            | :? uint as x -> x.ToString("X")
+            | :? uint64 as x -> x.ToString("X")
+            | :? string as s ->
+                s
+                |> validateStr
+                |> getValue
+                |> removeTrailingZeros
+            | _ -> invalidArg (nameof s) $"Only byte, uint, uint64 and string can be converted to a memory address."
+
+        MemoryAddress.Create(v.ToLower())
--- Types/MemoryAddress.fsi
+++ Types/MemoryAddress.fsi
@@ -1,20 +1,22 @@
 ﻿namespace DMLib.Types
 
-/// <summary>An hexadecimal value.</summary>
-/// <remarks>This type has basic support for comparing it to <c>int</c> types, but it may cause problems when comparing negative numbers, since it is not the intention for this type to store negative numbers, but memory addresses.</remarks>
+/// <summary>A positive hexadecimal value.</summary>
+/// <remarks>This type will always be compared as a <c>uint64</c> to avoid overflow exceptions, but functions for converting to other types are provided for convenience.</remarks>
 [<Sealed>]
-type HexNumber =
+type MemoryAddress =
     interface System.IComparable
     override Equals: a: obj -> bool
     override GetHashCode: unit -> int
+    member ToByte: unit -> byte
     member ToInt: unit -> int
     member ToInt64: unit -> int64
-    override ToString: unit -> string
-    static member Create: x: string -> HexNumber
+    member ToUInt: unit -> uint32
+    member ToUInt64: unit -> uint64
+    static member Create: x: string -> MemoryAddress
 
 [<AutoOpen>]
-module HexNumberTopLevelOperations =
+module MemoryAddressTopLevelOperations =
     ///<summary> Creates a new <c>HexNumber</c> from a <c>string</c>, <c>int</c> or <c>int64</c>.
     ///Using a leading <c>0x</c> on <c>string</c> inputs is optional.</summary>
     ///<exception cref="T:System.ArgumentException">When the input is not a supported integer type or a valid string.</exception>
-    val inline HexNumber: s: obj -> HexNumber
+    val MemoryAddress: s: obj -> MemoryAddress
